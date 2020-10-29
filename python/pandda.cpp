7cb2d5d1db37c38dae9f435d4c424a03655293fa
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2020-10-29 23:51:56 +0000

    t

--- python/pandda.cpp
+++ python/pandda.cpp
@@ -1,5 +1,7 @@
 // Copyright 2018 Global Phasing Ltd.
 
+#include <iostream>
+
 #include "gemmi/ccp4.hpp"
 #include "gemmi/gz.hpp"  // for MaybeGzipped
 #include "gemmi/neighbor.hpp"
@@ -37,6 +39,7 @@ Grid<float> interpolate_points(
     for (std::size_t i=0; i < point_vec.size(); i++)
     {
         // Position
+        cout << "Getting postion\n";
         std::vector<int> point = point_vec[i];
         /*
         std::vector<float> pos_python = pos_vec[i];
@@ -46,22 +49,30 @@ Grid<float> interpolate_points(
             pos_python[2]
             );
         */
+        cout << "Getting gemmi point\n";
         auto point_gemmi = interpolated_map.get_point(point[0], point[1], point[2]);
-        Position pos = interpolated_map.point_to_position(point_gemmi); 
+        cout << "Getting gemmi position\n";
+        Position pos = interpolated_map.point_to_position(point_gemmi);
+        cout << "Getting transform\n"; 
         Transform transform = transform_vec[i];
+        cout << "Getting com moving\n"; 
         std::vector<float> com_moving = com_moving_vec[i];
+        cout << "Getting reference\n"; 
         std::vector<float> com_reference = com_reference_vec[i];
 
 
         //Subtract reference com
+        cout << "Subtracting reference\n"; 
         pos.x -= com_reference[0];
         pos.y -= com_reference[1];
         pos.z -= com_reference[2];
 
         //transform
+        cout << "Transforming\n"; 
         Position pos_moving = Position(transform.apply(pos));
 
         // add moving com
+        cout << "Adding moving\n"; 
         pos_moving.x += com_moving[0];
         pos_moving.y += com_moving[1];
         pos_moving.z += com_moving[2];
@@ -70,9 +81,11 @@ Grid<float> interpolate_points(
         //Fractional pos_moving_fractional = moving_map.unit_cell.fractionalize(pos_moving);
 
         // interpolate
+        cout << "interpolating\n"; 
         float interpolated_value = moving_map.interpolate_value(pos_moving);
 
         // assign
+        cout << "Assigning\n"; 
         interpolated_map.set_value(
             point[0],
             point[1],
